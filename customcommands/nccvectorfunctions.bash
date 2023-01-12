a41774cc8f72c02cd179de8ad5e6450299ba69f8
Faizan Ali <fzn1020@gmail.com>
2023-01-12 22:19:30 +0900

    functions update

--- customcommands/nccvectorfunctions.bash
+++ customcommands/nccvectorfunctions.bash
@@ -9,3 +9,13 @@ function gcops {
     git push
 }
 
+# Git commit, push and set upstream
+function gcosu{
+    git pull
+    git add .
+    git commit -m "$1"
+    BRANCH_NAME = ${git symbolic-ref --short -q HEAD}
+    echo "Upstreaming $BRANCH_NAME"
+    git push -u origin/$BRANCH_NAME
+}
+
