7bc5a2964773076d356cfca63db5ebf43918222b
Faizan Ali <fzn1020@gmail.com>
2023-01-12 21:41:18 +0900

    env update

--- nccvector_env.bash
+++ nccvector_env.bash
@@ -1,9 +1,4 @@
 #!/bin/bash -i
 
-# Go to customcommands directory
-cd configs/customcommands
-NCCVECTOR_PWD = $pwd
-
 # Sourcing all the shit
-# for f in ~/.bash_profile_*; do source $f; done
-for f in ./*; do source $f; done
+for f in ./customcommands/*; do source $f; echo "sourced $f"; done
