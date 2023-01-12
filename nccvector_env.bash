740c803fa4dfafe2c3c10251a63e43ce922ca76e
Faizan Ali <fzn1020@gmail.com>
2023-01-12 22:01:44 +0900

    env update

--- nccvector_env.bash
+++ nccvector_env.bash
@@ -1,7 +1,8 @@
 #!/bin/bash -i
 
 echo "$BASH_SOURCE"
-echo "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "$SCRIPT_DIR"
 
 # Sourcing all the shit
 for f in ./customcommands/*; do source $f; echo "sourced $f"; done
